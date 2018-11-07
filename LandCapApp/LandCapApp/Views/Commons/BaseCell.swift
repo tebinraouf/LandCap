d568b5d89374627ab173a966173af343d326c54a
Tebin <tebin.raouf@gmail.com>
2018-11-06 19:02:16 -0500

    add BaseView and BaseCell docs

--- LandCapApp/LandCapApp/Views/Commons/BaseCell.swift
+++ LandCapApp/LandCapApp/Views/Commons/BaseCell.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
-
+///A utility class derived from UICollectionViewCell for all UICollectionView cells
 class BaseCell: UICollectionViewCell {
+    ///Main constructor
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
     }
+    ///A starting point for all classes that use `BaseCell` as the base class.
     func setupView() {
         //setup views here
     }
+    ///Required for UICollectionViewCell
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- LandCapApp/LandCapApp/Views/Commons/BaseView.swift
+++ LandCapApp/LandCapApp/Views/Commons/BaseView.swift
@@ -7,21 +7,19 @@
 //
 
 import UIKit
-
+///A utility class derived from UIView for all UIView cells
 class BaseView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
-
         setupView()
     }
-    
+    ///A starting point for all classes that use `BaseView` as the base class.
     func setupView() {
-        
+        //to be overridden by child classes
     }
-    
+    ///Required for UIView
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
